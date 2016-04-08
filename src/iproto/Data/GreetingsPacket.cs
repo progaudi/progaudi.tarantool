b92b7c81dd3ce2725733eeff3d3c167e531c19ec
roman-kozachenko <roman.kozachenko@gmail.com>
2016-04-08 16:03:41 +0300

    - IProto project added;  - data models added;

---
+++ src/iproto/Data/GreetingsPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace iproto.Data
+{
+    public class GreetingsPacket
+    {
+        public GreetingsPacket(byte[] message, byte[] salt)
+        {
+            Message = message;
+            Salt = salt;
+        }
+
+        public byte[] Message { get; }
+
+        public byte[] Salt { get; }
+    }
+}
