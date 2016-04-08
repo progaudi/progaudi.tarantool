ec8af033d0f75fb9590ee20ad91768d81ddedf39
aensidhe <me@aensidhe.ru>
2016-04-08 13:12:24 +0300

    Port to dnx.

---
+++ src/msgpack/Interfaces/IBytesReader.cs
@@ -0,0 +1,17 @@
+﻿namespace TarantoolDnx.MsgPack.Interfaces
+{
+    public interface IBytesReader
+    {
+        sbyte ReadSByte();
+        byte ReadByte();
+        short ReadInt16();
+        ushort ReadUInt16();
+        int ReadInt32();
+        uint ReadUInt32();
+        long ReadInt64();
+        ulong ReadUInt64();
+        double ReadDouble();
+        float ReadFloat();
+        byte[] ReadBytes(int count);
+    }
+}
---
+++ tests/msgpack.tests/Reader/BaseReaderTest.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using TarantoolDnx.MsgPack.Interfaces;
+
+namespace TarantoolDnx.MsgPack.Tests.Reader
+{
+    public class BaseReaderTest
+    {
+        protected IMsgPackReader CreateReader(byte[] data)
+        {
+            var stream = new MemoryStream(data);
+            return new MsgPackReader(stream);
+        }
+    }
+}
