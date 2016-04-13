926d24941008f56fc311d20d3e582dd8566e2368
roman-kozachenko <roman.kozachenko@gmail.com>
2016-04-13 17:19:13 +0300

    DateTimeConverter and its tests created

---
+++ tests/msgpack.tests/Writer/DateTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+using Shouldly;
+
+using TarantoolDnx.MsgPack.Converters;
+
+using Xunit;
+
+namespace TarantoolDnx.MsgPack.Tests.Writer
+{
+    public class DateTime
+    {
+        [Fact]
+        public void TestDateTime()
+        {
+            var tests = new List<KeyValuePair<System.DateTime, byte[]>>()
+            {
+                new KeyValuePair<System.DateTime, byte[]>(System.DateTime.MinValue, new byte[] {0,}),
+                new KeyValuePair<System.DateTime, byte[]>(System.DateTime.MaxValue, new byte[] {211, 43, 202, 40, 117, 244, 55, 63, 255,}),
+                new KeyValuePair<System.DateTime, byte[]>(new System.DateTime(), new byte[] {0,}),
+                new KeyValuePair<System.DateTime, byte[]>(new System.DateTime(1, 1, 1), new byte[] {0,}),
+                new KeyValuePair<System.DateTime, byte[]>(new System.DateTime(2015, 11, 17), new byte[] {211, 8, 210, 238, 226, 9, 96, 128, 0,}),
+                new KeyValuePair<System.DateTime, byte[]>(new System.DateTime(1, 2, 3, 4, 5, 6, DateTimeKind.Utc), new byte[] {211, 0, 0, 26, 16, 181, 23, 69, 0,}),
+            };
+
+            foreach (var test in tests)
+            {
+                MsgPackConverter.Serialize(test.Key).ShouldBe(test.Value);
+            }
+        }
+    }
+}
