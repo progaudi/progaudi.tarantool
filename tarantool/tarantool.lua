e901b16cbcdb5ad0671066e1319af066e8647006
Anatoly Popov <aensidhe@users.noreply.github.com>
2016-10-21 14:34:04 +0300

    Fix bug

--- tarantool/tarantool.lua
+++ tarantool/tarantool.lua
@@ -9,18 +9,17 @@ local function init()
     space1 = box.schema.space.create('primary_only_index', { if_not_exists = true })
     space1:create_index('primary', {type='hash', parts={1, 'unsigned'}, if_not_exists = true})
 
-    performanceSpace = box.schema.space.create('performance')
+    performanceSpace = box.schema.space.create('performance', { if_not_exists = true })
     performanceSpace:create_index('primary', {type='hash', parts={1, 'unsigned'}, if_not_exists = true})
 
-
-    space2 = box.schema.space.create('primary_and_secondary_index')
+    space2 = box.schema.space.create('primary_and_secondary_index', { if_not_exists = true })
     space2:create_index('hashIndex', {type='hash',  parts={1, 'unsigned'}, if_not_exists = true })
     space2:create_index('treeIndex', {type='tree',  parts={1, 'unsigned'}, if_not_exists = true })
 
     box.schema.user.create('notSetPassword', { if_not_exists = true })
-    box.schema.user.create('emptyPassword', {password = '', if_not_exists = true})
+    box.schema.user.create('emptyPassword', { password = '', if_not_exists = true })
 
-    box.schema.user.create('operator', {password = 'operator', if_not_exists = true})
+    box.schema.user.create('operator', {password = 'operator', if_not_exists = true })
     box.schema.user.grant('operator','read,write,execute','universe', { if_not_exists = true })
 
     function log_connect ()
