433165e6edd4e6d5494a173b10ea81f3777dd457
Anatoly Popov <aensidhe@users.noreply.github.com>
2016-10-29 22:03:52 +0300

    Try to use sudo to install docker

--- .travis/osx/init.sh
+++ .travis/osx/init.sh
@@ -2,6 +2,6 @@
 
 echo 'Mac init script'
 brew tap caskroom/cask
-brew cask install docker
+sudo brew cask install docker
 find /usr/local/Caskroom/docker
 ls /usr/local/Caskroom/docker/1.12.0.9996/Applications
