ee12711bacaee94f6c2879d71cc4f51e4aeb8cc6
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-08-31 18:32:49 +0300

    Update build scripts

--- scripts/build-netcore.sh
+++ scripts/build-netcore.sh
@@ -8,7 +8,9 @@ cd ..
 
 dotnet restore
 dotnet build -c Release -f netstandard1.4 src/progaudi.tarantool/progaudi.tarantool.csproj
+dotnet build -c Release -f netstandard2.0 src/progaudi.tarantool/progaudi.tarantool.csproj
 dotnet build -c Release -f netcoreapp1.0 tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj
 dotnet build -c Release -f netcoreapp1.1 tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj
+dotnet build -c Release -f netcoreapp2.0 tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj
 
 popd
--- scripts/dotnet-install.sh
+++ scripts/dotnet-install.sh
@@ -4,5 +4,5 @@ set -ev
 
 curl -o ./dotnet-install.sh -fL https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh
 chmod +x ./dotnet-install.sh
-version=$(jq -r .sdk.version global.json)
-./dotnet-install.sh --version $version
+./dotnet-install.sh --version 1.0.4
+./dotnet-install.sh --version 2.0.0
