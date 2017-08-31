9edffcad054fbdd1e9268cff2dca4ad1b1d54b41
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-08-31 17:09:29 +0300

    Add simple benchmarking

---
+++ src/progaudi.tarantool.benchmark/IncrementBenchmark.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using ProGaudi.Tarantool.Client;
+using ProGaudi.Tarantool.Client.Model.Responses;
+using StackExchange.Redis;
+
+namespace progaudi.tarantool.benchmark
+{
+    [Config(typeof(BenchmarkConfig))]
+    public class IncrementBenchmark
+    {
+        private readonly Box _box;
+        private readonly IDatabaseAsync _redis;
+
+        public IncrementBenchmark()
+        {
+            _box = Box.Connect("localhost", 3301).GetAwaiter().GetResult();
+            _redis = ConnectionMultiplexer.Connect("localhost:6379").GetDatabase();
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task<long> Redis() => await _redis.StringIncrementAsync("test_for_benchmarking");
+
+        [Benchmark]
+        public async Task<DataResponse<int[]>> Tarantool() => await _box.Call<int>("test_for_benchmarking");
+    }
+}
---
+++ src/progaudi.tarantool.benchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace progaudi.tarantool.benchmark
+{
+    public static class Program
+    {
+        public static void Main() => BenchmarkRunner.Run<IncrementBenchmark>();
+    }
+}
