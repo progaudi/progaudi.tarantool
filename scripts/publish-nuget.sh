1f5a0e2c5ff327476df042af5bc08b2c3d06c0ed
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-11-19 21:38:54 +0300

    Let us debug this script on teamcity

--- scripts/publish-nuget.sh
+++ scripts/publish-nuget.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e -x
+
 version=$(git describe --tags | sed s/-/./)
 dotnet_path=/$(echo $1 | sed 's/\\/\//g' | sed 's/://')
 ./scripts/if_there_are_changes_in.sh 'src/progaudi.tarantool' --then $dotnet_path/dotnet pack --include-symbols -c Release -o ../../Publish src/progaudi.tarantool/progaudi.tarantool.csproj /property:Version=$version
