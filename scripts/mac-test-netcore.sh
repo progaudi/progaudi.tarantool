490279038c2c34d48a7f1eea9506ad3793346939
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-11-20 15:35:45 +0300

    Kill a specific process by pid

--- scripts/mac-test-netcore.sh
+++ scripts/mac-test-netcore.sh
@@ -5,11 +5,14 @@ set -e -v -x
 pushd ${BASH_SOURCE%/*}/..
 
 dotnet test -c Release --no-build --filter "Tarantool!=1.8" tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj -- -parallel assemblies
-killall -9 tarantool
+
+pushd tarantool
+
+kill -KILL $(cat tarantool.pid)
+
 brew uninstall tarantool
 brew install tarantool --HEAD
 
-pushd tarantool
 tarantool tarantool.lua
 dotnet test -c Release --no-build --filter "Tarantool==1.8" tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj -- -parallel assemblies
 
