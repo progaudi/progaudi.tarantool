e96be7cd858c6b01316e2a563554cef79cb25372
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-11-20 21:51:24 +0300

    Fix path for test runner

--- scripts/mac-test-netcore.sh
+++ scripts/mac-test-netcore.sh
@@ -14,8 +14,9 @@ brew uninstall tarantool
 brew install tarantool --HEAD
 
 tarantool tarantool.lua
+
+popd
+
 dotnet test -c Release --no-build --filter "Tarantool=1.8" tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj -- -parallel assemblies
 
 popd
-popd
-
