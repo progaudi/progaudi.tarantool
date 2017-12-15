0f8048773c3ca0b8b132284f6ded280db52cf8ee
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-12-15 09:33:26 +0300

    Run only 1.8 tests on mac-1.8

--- scripts/mac-test-netcore.sh
+++ scripts/mac-test-netcore.sh
@@ -4,4 +4,4 @@ set -e -v -x
 
 pushd ${BASH_SOURCE%/*}/..
 
-dotnet test -c Release --no-build $TEST_FILTER tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj -- -parallel assemblies
+TARANTOOL_1_7_REPLICATION_SOURCE=localhost:3301 TARANTOOL_1_8_REPLICATION_SOURCE=localhost:3301 dotnet test -c Release --no-build $TEST_FILTER tests/progaudi.tarantool.tests/progaudi.tarantool.tests.csproj -- -parallel assemblies
