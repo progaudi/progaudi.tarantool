4621da0408964dd51ddbc06260bf8e9a0786bde1
aensidhe <me@aensidhe.ru>
2018-05-24 18:21:22 +0300

    Add insert performance benchmark. 21711 ms for 1M inserts

---
+++ samples/insert-performance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using ProGaudi.Tarantool.Client;
+using ProGaudi.Tarantool.Client.Model;
+
+namespace Tarantool.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var box = Box.Connect("localhost:3301").GetAwaiter().GetResult())
+            {
+                var schema = box.GetSchema();
+                var space = schema["pivot"];
+                var lst = new Task[1000];
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < 1_000_000; i++)
+                {
+                    lst[i % 1000] = space.Insert((i, (i, i), i));
+                    
+                    if (i % 1000 == 999)
+                    {
+                        Task.WhenAll(lst);
+                    }
+
+                    if (i % 10000 == 9999)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                sw.Stop();
+
+                Console.WriteLine();
+                Console.WriteLine(sw.ElapsedMilliseconds);
+            }
+        }
+    }
+}
--- tarantool/testdata.lua
+++ tarantool/testdata.lua
@@ -1,26 +1,24 @@
 local log = require("log")
-local remote = require('net.box')
-local compat = require('compat')
+local remote = require("net.box")
+local compat = require("compat")
 
 local new_parts_format_supported = compat.check_version{1, 7, 6}
 local is_nullable_supported = compat.check_version{1, 7, 6}
 
 local function create_space(space)
-	log.info{message2="Creating space.", name=space.name}
-	local format = {}
-	for name, field in pairs(space.fields) do
-		format[field.index] = { name=name, type=field.type }
-		if is_nullable_supported then
-			format[field.index].is_nullable = false
-			if field.is_nullable then
-				format[field.index].is_nullable = true
-			end
-		end
-	end
+    log.info({space=space.name, status="creating"})
+    local format = {}
+    for name, field in pairs(space.fields) do
+        format[field.index] = { name=name, type=field.type, is_nullable=(field.is_nullable == true) }
+    end
 
-	local created_space = box.schema.space.create(space.name, { format = format, if_not_exists=true, field_count=#format })
-	log.info{message2="Created space.", name=space.name}
-	return created_space
+    if space.sequence ~= nil then
+        box.schema.sequence.create(space.sequence, {if_not_exists = true})
+    end
+
+    local created_space = box.schema.space.create(space.name, { format = format, if_not_exists = true, temporary = space.temporary})
+    log.info({space=space.name, status="created"})
+    return created_space
 end
 
 local function create_index(space, name, unique, type, sequence, ...)
@@ -42,93 +40,147 @@ local function create_index(space, name, unique, type, sequence, ...)
 		sequence = sequence
 	}
 
-	log.info{message2="Creating index", space=space.name, name=name, options=options}
-
 	local index = space:create_index(name, options)
 
-	log.info{message2="Created index", space=space.name, name=name}
 	return index
+end
+
+local function create_index(space, name, unique, type, sequence_name, ...)
+    log.info({space=space.name, index=name, status="creating"})
+    local parts = {}
+
+    for k, v in pairs({...}) do
+        table.insert(parts, v)
+    end
+
+    local options = {
+        unique = unique,
+        type = type,
+        parts = parts,
+        if_not_exists = true,
+    }
+
+    log.info({space=space.name, index=name, status="options", options=options})	
+    local index = space:create_index(name, options)
+    if sequence_name ~= nil then
+        index:alter({sequence = sequence_name})
+    end
+
+    log.info({space=space.name, index=name, status="created"})
+    return index
 end
 
 local spaces = {
 	primary_only_index = {
 		name = "primary_only_index",
 		fields = {
-			id =      { index = 1, name="id", type = "unsigned" },
-			name =    { index = 2, name="name", type = "string" },
-			price =   { index = 3, name="price", type = "scalar", is_nullable=true }
+			id =      { index = 1, type = "unsigned" },
+			name =    { index = 2, type = "string" },
+			price =   { index = 3, type = "scalar", is_nullable=true }
 		}
 	},
 	performance = {
 		name = "performance",
 		fields = {
-			id =      { index = 1, name="id", type = "unsigned" },
-			name =    { index = 2, name="name", type = "string" }
+			id =      { index = 1, type = "unsigned" },
+			name =    { index = 2, type = "string" }
 		}
 	},
 	treeIndexMethods = {
 		name = "space_TreeIndexMethods",
+		sequence = "space_TreeIndexMethods_id",
 		fields = {
-			id =      { index = 1, name="id", type = "unsigned" },
-			name =    { index = 2, name="name", type = "string" }
+			id =      { index = 1, type = "unsigned" },
+			name =    { index = 2, type = "string" }
+		}
+	},
+	with_scalar_index = {
+		name = "with_scalar_index",
+		fields = {
+			id =      { index = 1, type = "scalar" }
+		}
+	},
+	pivot = {
+		name = "pivot",
+		temporary = true,
+		fields = {
+			id =      { index = 1, type = "unsigned"},
+			arr =     { index = 2, type = "array" }
 		}
 	}
 }
 
 local function create_spaces_and_indecies()
 	local space = create_space(spaces.primary_only_index)
-	create_index(space, "primary", true, "HASH", nil, spaces.primary_only_index.fields.id)
+	create_index(space, "primary", true, "HASH", nil, "id")
 
 	space = create_space(spaces.performance)
-	create_index(space, "primary", true, "HASH", nil, spaces.performance.fields.id)
+	create_index(space, "primary", true, "HASH", nil, "id")
 
-	space = box.schema.space.create('with_scalar_index', { if_not_exists = true })
-	space:create_index('primary', {type='tree', parts={1, 'scalar'}, if_not_exists = true})
+	space = create_space(spaces.with_scalar_index)
+	create_index(space, "primary", true, "TREE", nil, "id")
+
+	space = create_space(spaces.pivot)
+	create_index(space, "primary", true, "TREE", nil, "id")
+	create_index(space, "rtree", false, "RTREE", nil, "arr")
+
+	space = create_space(spaces.treeIndexMethods)
+	create_index(space, "primary", true, "TREE", spaces.treeIndexMethods.sequence, "id")
+
+	space:insert{nil, "asdf"}
+	space:insert{nil, "zcxv"}
+	space:insert{nil, "qwer"}
 end
 
 local function init()
+	log.info{stage="Spaces",status="begin"}
 	create_spaces_and_indecies()
+	log.info{stage="Spaces",status="end"}
 
-	box.schema.user.create('notSetPassword', { if_not_exists = true })
-	box.schema.user.create('emptyPassword', { password = '', if_not_exists = true })
+	log.info{stage="Users",status="begin"}
+	box.schema.user.create("notSetPassword", { if_not_exists = true })
+	box.schema.user.create("emptyPassword", { password = "", if_not_exists = true })
 
-	box.schema.user.create('operator', {password = 'operator', if_not_exists = true })
-	box.schema.user.grant('operator','read,write,execute','universe', { if_not_exists = true })
-	box.schema.user.grant('guest','read,write,execute','universe', { if_not_exists = true })
-	box.schema.user.grant('emptyPassword','read,write,execute','universe', { if_not_exists = true })
-	box.schema.user.passwd('admin', 'adminPassword')
+	box.schema.user.create("operator", {password = "operator", if_not_exists = true })
+	log.info{stage="Users",status="end"}
+	
+	log.info{stage="Grants",status="begin"}
+
+	local users = {"operator", "guest", "emptyPassword"}
+
+	for _, user in pairs(users) do
+		box.schema.user.grant(user, "execute", "universe", nil, { if_not_exists = true })
+
+		for _, space in pairs(spaces) do
+			box.schema.user.grant(user, 'read,write', 'space', space.name, {if_not_exists = true})
+			if space.sequence ~= nil then
+				box.schema.user.grant(user, 'read,write', 'sequence', space.sequence, {if_not_exists = true})
+			end
+		end
+	end
+
+	log.info{stage="Grants",status="end"}
 end
 
-local function space_TreeIndexMethods()
-	local sequence = box.schema.sequence.create('space_TreeIndexMethods_id')
-	local space = create_space(spaces.treeIndexMethods)
-	create_index(space, "treeIndex", true, "TREE", sequence.name, spaces.treeIndexMethods.fields.id)
+box.once("init", init)
 
-	space:insert{nil, 'asdf'}
-	space:insert{nil, 'zcxv'}
-	space:insert{nil, 'qwer'}
-end
-
-box.once('init', init)
-box.once('space_TreeIndexMethods', space_TreeIndexMethods)
-
-local log = require('log')
+local log = require("log")
 
 function log_connect ()
-	local m = 'Connection. user=' .. box.session.user() .. ' id=' .. box.session.id()
+	local m = "Connection. user=" .. box.session.user() .. " id=" .. box.session.id()
 	log.info(m)
 end
 function log_disconnect ()
-	local m = 'Disconnection. user=' .. box.session.user() .. ' id=' .. box.session.id()
+	local m = "Disconnection. user=" .. box.session.user() .. " id=" .. box.session.id()
 	log.info(m)
 end
 
 function log_auth ()
-	local m = 'Authentication attempt'
+	local m = "Authentication attempt"
 	log.info(m)
 end
 function log_auth_ok (user_name)
-	local m = 'Authenticated user ' .. user_name
+	local m = "Authenticated user " .. user_name
 	log.info(m)
 end
 
@@ -138,33 +190,33 @@ box.session.on_auth(log_auth)
 box.session.on_auth(log_auth_ok)
 
 function return_null()
-	log.info('return_null called')
-	return require('msgpack').NULL
+	log.info("return_null called")
+	return require("msgpack").NULL
 end
 
 function return_tuple_with_null()
-	log.info('return_tuple_with_null called')
-	return { require('msgpack').NULL }
+	log.info("return_tuple_with_null called")
+	return { require("msgpack").NULL }
 end
 
 function return_tuple()
-	log.info('return_tuple called')
+	log.info("return_tuple called")
 	return { 1, 2 }
 end
 
 
 function return_array()
-	log.info('return_array called')
+	log.info("return_array called")
 	return {{ "abc", "def" }}
 end
 
 function return_scalar()
-	log.info('return_scalar called')
+	log.info("return_scalar called")
 	return 1
 end
 
 function return_nothing()
-	log.info('return_nothing called')
+	log.info("return_nothing called")
 end
 
 local truncate_space = function(name)
@@ -180,16 +232,16 @@ local truncate_space = function(name)
 end
 
 function create_sql_test()
-	box.sql.execute('create table sql_test(id int primary key, name text)')
+	box.sql.execute("create table sql_test(id int primary key, name text)")
 	box.sql.execute("insert into sql_test values (1, 'asdf'), (2, 'zxcv'), (3, 'qwer')")
 end
 
 function drop_sql_test()
-	box.sql.execute('drop table sql_test')
+	box.sql.execute("drop table sql_test")
 end
 
 function clear_data(spaceNames)
-	log.info('clearing data...')
+	log.info("clearing data...")
 	for _, spaceName in ipairs(spaceNames) do
 		truncate_space(spaceName)
 	end
