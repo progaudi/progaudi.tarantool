9a1c4b2e44297dcc0dd8f49c1f1c193d07f4cc08
aensidhe <me@aensidhe.ru>
2018-06-19 01:29:37 +0300

    Baseline

---
+++ samples/insert-performance/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using ProGaudi.Tarantool.Client;
+using ProGaudi.Tarantool.Client.Model;
+
+namespace Tarantool.Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            var log = new TextWriterLog(Console.Out);
+            var options = new ClientOptions(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "localhost:3301" : "tarantool_1_8:3301");
+            //var options = new ClientOptions(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "localhost:3301" : "tarantool_1_8:3301", log);
+            var sw = new Stopwatch();
+            try
+            {
+                using (var box = new Box(options))
+                {
+                    box.Connect().GetAwaiter().GetResult();
+                    var space = box.Schema["pivot"];
+                    var lst = new Task[1000];
+                    sw.Start();
+                    for (var i = 0; i < 1_000_000; i++)
+                    {
+                        lst[i % 1000] = space.Insert((i, (i, i), i));
+
+                        if (i % 1000 == 999)
+                        {
+                            Task.WaitAll(lst);
+                            //return;
+                        }
+
+                        if (i % 10000 == 9999)
+                        {
+                            Console.Write("*");
+                            if (i % 100000 == 99999)
+                            {
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                sw.Stop();
+
+                Console.WriteLine();
+                Console.WriteLine(sw.ElapsedMilliseconds);
+            }
+        }
+    }
+}
