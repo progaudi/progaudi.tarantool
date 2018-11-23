1f1bbd1681ee80637a89ffa575f2bba9a7478030
Anatoly Popov <aensidhe@users.noreply.github.com>
2018-11-23 16:02:55 +0300

    InsertReplaceRequest fixed.

--- src/progaudi.tarantool/Model/Requests/InsertReplaceRequest.cs
+++ src/progaudi.tarantool/Model/Requests/InsertReplaceRequest.cs
@@ -1,16 +1,24 @@
-﻿using ProGaudi.MsgPack;
+﻿using System;
+using ProGaudi.MsgPack;
 using ProGaudi.Tarantool.Client.Model.Enums;
 
 namespace ProGaudi.Tarantool.Client.Model.Requests
 {
     public abstract class InsertReplaceRequest<T> : Request
     {
+        private readonly IMsgPackFormatter<InsertReplaceRequest<T>> _formatter;
+
         protected InsertReplaceRequest(CommandCode code, uint spaceId, T tuple, MsgPackContext context)
             : base(code, context)
         {
             SpaceId = spaceId;
             Tuple = tuple;
+            _formatter = context.GetRequiredFormatter<InsertReplaceRequest<T>>();
         }
+
+        protected override int GetApproximateBodyLength() => _formatter.GetBufferSize(this);
+
+        protected override int WriteBodyTo(Span<byte> buffer) => _formatter.Format(buffer, this);
 
         public uint SpaceId { get; }
 
--- src/progaudi.tarantool/Model/Requests/InsertRequest.cs
+++ src/progaudi.tarantool/Model/Requests/InsertRequest.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using ProGaudi.MsgPack;
+﻿using ProGaudi.MsgPack;
 using ProGaudi.Tarantool.Client.Model.Enums;
 
 namespace ProGaudi.Tarantool.Client.Model.Requests
 {
     public class InsertRequest<T> : InsertReplaceRequest<T>
     {
-        private readonly IMsgPackFormatter<InsertRequest<T>> _formatter;
-
         public InsertRequest(uint spaceId, T tuple, MsgPackContext context)
             : base(CommandCode.Insert, spaceId, tuple, context)
         {
-            _formatter = context.GetRequiredFormatter<InsertRequest<T>>();
         }
-
-        protected override int GetApproximateBodyLength() => _formatter.GetBufferSize(this);
-
-        protected override int WriteBodyTo(Span<byte> buffer) => _formatter.Format(buffer, this);
     }
 }
