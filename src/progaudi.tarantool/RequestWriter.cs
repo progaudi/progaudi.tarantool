6a9eeec65126ae16ef9890695b560678b2f8dc7b
Nick Karlov <karlovnv@gmail.com>
2019-09-12 11:31:12 +0300

    remove c# 7.1 feature in order to build

--- src/progaudi.tarantool/RequestWriter.cs
+++ src/progaudi.tarantool/RequestWriter.cs
@@ -131,7 +131,7 @@ namespace ProGaudi.Tarantool.Client
                     }
                 }
 
-                result = default;
+                result = default(ArraySegment<byte>);
                 return false;
              }
 
